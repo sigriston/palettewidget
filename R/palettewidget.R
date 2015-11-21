0d1384091c56296431ae92e17134768d339891b7
Thiago Sigrist <sigrist@gmail.com>
2015-11-21 10:42:49 -0200

    documentation

--- R/palettewidget.R
+++ R/palettewidget.R
@@ -1,3 +1,19 @@
+#' Color Palette Widget
+#'
+#' Displays a color palette and their HTML/CSS hex color codes or names.
+#'
+#' @param palette The color palette to be displayed.
+#'
+#' @param displayNames Use `TRUE` to attempt to display HTML/CSS color names
+#'                     for those colors that have them.
+#'
+#' @return The rendered color palette widget.
+#'
+#' @examples
+#' palettewidget(c("#ff0000", "#00ff00", "#0000ff"))
+#' palettewidget(c("#ff0000", "#00ff00", "#0000ff"),
+#'               displayNames = TRUE)  # should display "red", "lime", "blue"
+#'
 #' @import htmlwidgets
 #' @export
 palettewidget <- function(palette, displayNames = FALSE) {
